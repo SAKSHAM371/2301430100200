0dd3af98d2838886fc4212ec03f4e157954bb179
saksham <saksham170606@gmail.com>
2026-06-08 12:58:56 +0530

    saksham

---
+++ src/services/api.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const BASE_URL =
+  "http://4.224.186.213/evaluation-service";
+
+const TOKEN = import.meta.env.VITE_ACCESS_TOKEN;
+
+console.log("TOKEN =", TOKEN);
+console.log(
+  "AUTH HEADER =",
+  `Bearer ${TOKEN}`
+);
+
+export const getNotifications = async (
+  page = 1,
+  limit = 10,
+  type = ""
+) => {
+  const response = await axios.get(
+    `${BASE_URL}/notifications`,
+    {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+      params: {
+        page,
+        limit,
+        notification_type: type,
+      },
+    }
+  );
+
+  return response.data;
+};
